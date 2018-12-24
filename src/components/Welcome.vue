4f9893d839334250248d38668b2567fc0f3d5988
fakorede <abiolafakorede@gmail.com>
2018-12-24 02:59:04 +0100

    first commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container welcome">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+</style>
